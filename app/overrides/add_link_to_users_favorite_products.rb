c5f3e1de5832ce048a078c56bd977e0231f787d7
catalin88 <catalin_t_vulcan@yahoo.com>
2017-02-26 23:37:58 +0200

    Add number of favorites

--- app/overrides/add_link_to_users_favorite_products.rb
+++ app/overrides/add_link_to_users_favorite_products.rb
@@ -2,7 +2,6 @@ Deface::Override.new(
   virtual_path: "spree/shared/_nav_bar",
   name: "add_link_to_users_favorite_products",
   insert_before: "li#search-bar",
-  text: "<% if spree_current_user %><li><%= link_to 'Favorite Products', favorite_products_path %></li><% end %>",
+  text: "<li id='link-to-favorites'><%= link_to_favorites %></li>",
   sequence: { before: "auth_shared_login_bar"}
 )
-
